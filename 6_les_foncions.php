f271877d74fbddcc0a18d451ba21773dc6c0a7fd
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2023-11-13 14:02:09 +0100

    les fonctions php

---
+++ 6_les_foncions.php
@@ -0,0 +1,39 @@
+<?php
+
+    // fonction de php
+    /*
+        function functioName () {
+
+            return ;
+        }
+
+        function functioName (TypeArgument $arg1 = valeuPardefault): TypeReturn {
+
+            return typeReturn;
+        }
+
+        function some (int ...$numbers): int {
+
+            $result = 0;
+    
+            foreach ($numbers as $number) {
+                $result += $number;
+            }
+            
+            return $result;
+        }
+
+        $sum = some(1, 2, 3, 4); // 10
+
+    */
+
+
+    // notes :
+    /*
+        note 1 : la fonction est executer completment isole (on ne peut pas acceder les variable dehors la fonction)
+        note 2 : pour utiliser les variables globales on peut les passer comme des arguments ou utiliser mot_cle "global" 
+    */
+
+
+
+    
