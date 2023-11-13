4aa49f1c6dddffc39f96a0473d9981a1a451d0a2
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2023-11-13 13:11:51 +0100

    les boucles

---
+++ 5_les_boucles.php
@@ -0,0 +1,44 @@
+<?php
+
+
+    //while
+    /*
+        while(condition) {
+            // Code a repeter
+        }
+    */
+
+    
+    //do-while
+    /*
+        do {
+            // Code a repeter
+        } while(condition) 
+    */
+
+
+    //for
+    /*
+        for ($i = 0; $i < 100; $i++) {
+            // Code a repeter
+        }
+    */
+    
+    
+    //for-each
+    /*
+        foreach ( $notes as $note) {
+            // Code a repeter
+        }
+        
+        foreach ( $eleves as $nom => $note) {
+            // Code a repeter
+        }
+    */
+
+    
+
+    /*
+        break;
+        continue;
+    */
